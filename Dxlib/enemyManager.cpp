4b93620936cbb3de27c531787a4e706f8c140a39
k021g1165@g.neec.ac.jp <k021g1165@g.neec.ac.jp>
2022-10-10 20:36:57 +0900

    enemy完成前のmanager完成(enemyが完成したら修正箇所あり)

--- Dxlib/enemyManager.cpp
+++ Dxlib/enemyManager.cpp
@@ -1,1 +1,52 @@
 #include "enemyManager.h"
+
+enemyManager::enemyManager()
+{
+
+}
+
+enemyManager::~enemyManager()
+{
+
+	for (int i = 0; i < enemys.size(); i++)
+	{
+		delete enemys[i];
+	}
+
+}
+
+void enemyManager::update()
+{
+	for (int i = 0; i < enemys.size(); i++)
+	{
+		enemys[i]->Update();
+	}
+}
+
+void enemyManager::draw()
+{
+
+	for (int i = 0; i < enemys.size(); i++)
+	{
+		enemys[i]->Draw();
+	}
+
+}
+
+void enemyManager::enemyPop(float posX, float posY)
+{
+	//‰¼‰«‚Ì’†g‚ğì‚é
+	Enemy* newEnemy = new Enemy();
+
+	//enemy‚Ìinit‚ğŒÄ‚Ô
+	newEnemy->Initialize();
+
+	//Ši”[
+	enemys.push_back(newEnemy);
+
+}
+
+std::vector<Enemy*> enemyManager::getenemy()
+{
+	return enemys;
+}
--- Dxlib/enemyManager.h
+++ Dxlib/enemyManager.h
@@ -1,5 +1,30 @@
 #pragma once
+#include "Enemy.h"
+#include <vector>
+
 class enemyManager
 {
+public:
+	enemyManager();
+	~enemyManager();
+
+	void update();
+
+	void draw();
+
+	/// <summary>
+	/// “G¶¬
+	/// </summary>
+	/// <param name="posX">xÀ•W</param>
+	/// <param name="posY">yÀ•W</param>
+	void enemyPop(float posX,float posY);
+
+	//enemy‚ğ‚à‚ç‚¤ŠÖ”
+	std::vector<Enemy*> getenemy();
+
+private:
+	//enemy‚ğ‚Á‚Ä‚¢‚é‚â‚Â
+	std::vector<Enemy*> enemys;
+
 };
 
